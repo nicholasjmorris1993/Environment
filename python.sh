2ecd77cb3d4042c642f5fe7d34e22336fe7d7d59
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-12-21 11:52:29 -0500

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install python3.10-dev
 sudo apt-get install python3.10-venv
+sudo apt-get install glpk-utils libglpk-dev glpk-doc python3-swiglpk
 python3 -m venv /home/nick/env
 source /home/nick/env/bin/activate
 python3 -m pip install -r requirements.txt
